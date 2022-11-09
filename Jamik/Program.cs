5d431c04d905674ea6a47af97fcd574698fa730a
fif <fif@DESKTOP-BFHUIKE>
2022-11-10 08:57:35 +1200

    Добавьте файлы проекта.

---
+++ Jamik/Program.cs
@@ -0,0 +1,13 @@
+﻿//Скрипников Сергей 17 вар 3.3
+Console.WriteLine("Введите число n: ");
+double n = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число x: ");
+double x = double.Parse(Console.ReadLine());
+double s = 0;
+double z = 2;
+for (int i = 0; i < n; i += 1)
+{
+    z *= 2;
+    s = 1 + Math.Pow(x, z) / z!;
+}
+Console.WriteLine("ответ:" + s);  
